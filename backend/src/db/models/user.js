5fafa0ad2fec53d2d467de5ebf2a99db89605b14
little-Johny <molano.johny721@gmail.com>
2025-08-31 22:58:33 -0500

    feat: Construccion inicial de modelos

---
+++ backend/src/db/models/user.js
@@ -0,0 +1,50 @@
+"use strict";
+const { useDeferredValue } = require("react");
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+
+      // Un maestro puede crear muchos cursos
+      User.hasMany(models.Course, {
+        as: "courses",
+        foreignKey: "userId",
+      });
+
+      // Un usuario puede tener muchos favoritos
+      User.belongsToMany(models.Course, {
+        through: "UserFavorites",
+        as: "favorites",
+        foreignKey: "userId",
+      });
+    }
+  }
+  User.init(
+    {
+      name: { type: DataTypes.STRING, allowNull: false },
+      email: { type: DataTypes.STRING, unique: true, allowNull: false },
+      password: { type: DataTypes.STRING, allowNull: false },
+      role: {
+        type: DataTypes.ENUM("student", "teacher", "admin"),
+        defaultValue: "student",
+        allowNull: false,
+      },
+      theme: { type: DataTypes.BOOLEAN, allowNull: false, defaultValue: false }, // Por defecto sera
+    },
+    {
+      sequelize,
+      modelName: "User",
+      tableName: "Users",
+      timestamps: true,
+      paranoid: true,
+      deletedAt: "deletedAt",
+    }
+  );
+  return User;
+};
